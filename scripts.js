1461615bf2f18b63db63d15a7d7470afe69ed22a
Lucas Rodrigues <lucasrodriguessantos0@gmail.com>
2022-02-14 12:45:11 -0300

    Added an API

--- scripts.js
+++ scripts.js
@@ -1,14 +1,17 @@
 const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 
-const dolar = 5.28
-const euro = 5.98
-const bitcoin = 194542.81
 
-function convertMoney() {
+async function convertMoney() {
     const inputReais = document.getElementById('input-real').value
     const realValueText = document.getElementById('real-value-text')
     const currencyValueText = document.getElementById('currency-value-text')
+
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json()) //Utilizando um API
+
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = data.BTCBRL.high
 
     realValueText.innerHTML = new Intl.NumberFormat('pt-BR',{ style: 'currency', currency: 'BRL' }).format(inputReais);
 
